1b788aab25586a3af9e5087feb56e63c5f721f2d
Dr0zdd <ikantysh05@gmail.com>
2024-10-22 22:41:55 +0300

    fix: realize function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let str = Math.abs(n).toString();
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 }
+
+const reverse = require("./index");
+console.log(reverse(95034));
+
